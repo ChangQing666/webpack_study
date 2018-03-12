223ad7fb2f335adf517667a06e78bed5ae962e53
JYALL\chang.xianglin <chang.xianglin@jyall.com>
2018-03-12 15:39:46 +0800

    编写简单的react组件，将babel配置分离到.babelrc文件中

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,6 +1,22 @@
-var config = require('./config.json')
-module.exports = function(){
-    var greet = document.createElement('div')
-    greet.textContent = config.greetText
-    return greet;
+/*常规语法*/
+// var config = require('./config.json')
+// module.exports = function(){
+//     var greet = document.createElement('div')
+//     greet.textContent = config.greetText
+//     return greet;
+// }
+
+/*ES6语法*/
+import React,{Component} from 'react'
+import config from './config.json'
+
+class Greeter extends Component{
+    render(){
+        return (
+            <div>
+            {config.greetText}
+            </div>
+        )
+    }
 }
+export default Greeter
