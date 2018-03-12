31131a2cb13bb4d54e5573134a67f19b5a9dd472
JYALL\chang.xianglin <chang.xianglin@jyall.com>
2018-03-12 15:14:32 +0800

    webpack基础配置

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mode:'development',
+    devtool:'eval-source-map',
+    entry:__dirname+'/app/main.js',
+    output:{
+        path:__dirname+'public',
+        filename:'bundle.js'
+    },
+    devServer:{
+        contentBase:'./public',
+        historyApiFallback:true,
+        inline:true
+    }
+}
